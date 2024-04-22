87fe2b7ac710fb851ab86c960629626b0e48c70d
Narongchai (lmDOW) <bem255541@gmail.com>
2024-04-22 16:32:54 +0700

    add search input

--- src/Sales.jsx
+++ src/Sales.jsx
@@ -135,8 +135,12 @@ export default function Sales() {
 
                   
                 </div>
-                <div className="p-3 grid gap-2 grid-cols-1">
-                    <div className="p-5 bg-white rounded-md shadow-md text-center bg-white">
+                <div className="p-3 grid  gap-2 grid-cols-1">
+                    <label className="input input-bordered flex items-center ">
+                      <input type="text" className="grow" placeholder="Search" />
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                    </label>
+                    <div className="p-5 bg-white my-4 rounded-md shadow-md text-center bg-white">
                       <ProductSales />
                     </div>
                 </div>
